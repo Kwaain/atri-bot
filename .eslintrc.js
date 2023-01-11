2618100e8c54c9b5d820b8fd6dd0068973467e83
Kwaain <i@kwaain.com>
2023-01-11 21:39:05 +0800

    style: 优化 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,29 @@
-export default {
-    // 定义ESLint的解析器
-    parser: '@typescript-eslint/parser',
-    // 定义文件继承的子规范
-    extends: ['prettier/@typescript-eslint', 'plugin:prettier/recommended'],
-    // 定义了该eslint文件所依赖的插件
-    plugins: ['@typescript-eslint'],
-    // 指定代码的运行环境
-    env: {
-        node: true,
-    },
-}
+/** @format */
+
+import { defineConfig } from 'eslint-define-config'
+
+export default defineConfig({
+  root: true, // 指定当前配置文件为根配置文件，不再向上查找
+  // 指定环境的全局变量
+  env: {
+    ndoe: true, // Node.js 全局变量和 Node.js 作用域
+    es2021: true // 启用除了modules以外的所有ECMAScript 2021特性
+  },
+  // 继承的子规范
+  extends: [
+    'prettier/@typescript-eslint',
+    'standard',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  // 解析器选项
+  parserOptions: {
+    ecmaVersion: 13, // ECMAScript 版本
+    parser: '@typescript-eslint/parser', // 解析器
+    sourceType: 'module' // 指定来源的类型，有两种”script”或”module”
+  },
+  // 插件
+  plugins: ['@typescript-eslint', 'import', 'node', 'promise'],
+  // 自定义规则
+  rules: {}
+})
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-const ProjectName: string = 'atri-bot'
-
-function say (): string {
-  return `This project is ${ProjectName}.`
-}
-
-console.log(say())
+/** @format */
